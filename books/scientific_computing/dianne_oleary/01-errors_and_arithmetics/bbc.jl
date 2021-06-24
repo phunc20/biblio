c7a71cdfd4b618621aa49dd0c5d37f18650548f4
phunc20 <wucf20@gmail.com>
2021-06-24 20:36:02 +0700

    DONE plotting both well-/ill-conditioned plots

--- books/scientific_computing/dianne_oleary/01-errors_and_arithmetics/bbc.jl
+++ books/scientific_computing/dianne_oleary/01-errors_and_arithmetics/bbc.jl
@@ -33,7 +33,7 @@ end
 
 # ╔═╡ ce66f0b6-d4ca-11eb-3d3a-79e2988c7492
 md"""
-## 1.7  Conditioning and Stability
+## 1.7$(HTML("&ensp;")) Conditioning and Stability
 """
 
 # ╔═╡ 7896a3ef-ff1f-4840-86c4-8ec2938936ed
@@ -41,43 +41,95 @@ md"""
 
 # ╔═╡ ec6cddd4-e060-49c1-9080-cd254e0f3582
 md"""
-`δ` = $(@bind δ Slider(1e-6:1e-6:1e-3, show_value=true, default=1e-6))
+`δ` = $(@bind δ Slider(1e-5:1e-5:1e-3, show_value=true, default=1e-5))
 """
 
-# ╔═╡ 89a32942-b10c-4b07-b87a-a2a3cc1c2cdc
-range(0,1;length=10)
+# ╔═╡ 724c0e7a-8ec2-4f69-9142-5ecb4cb045b9
+δrange = range(δ,1,step=δ)
 
 # ╔═╡ d9ce9c5d-cd96-478f-8efd-b0cf4cc0aca0
 let
   #δrange = range(1e-6, 1e-2; length=100)
-  δrange = [10.0^(i) for i in -6:-2]
+  #δrange = [10.0^(i) for i in range(-6,0,step=δ)]
+  δrange = range(δ,1,step=δ)
   #δrange = range(δ, 1;length=10)
-  range_ = 0:.01:2
+  range_ = -1:.01:2
+
   md"""
-  This is a "scrubbable" matrix: click on the number and drag to change!
 
-  ``A_{\text{perturbed}} =``
+  ```math
+  Ax = b
+  ```
+
+  ``A =``
 
   ``(``
-  $(@bind a Scrubbable(δrange; format=".6f", default=10^(-4)))
-  $(@bind b Scrubbable(range_; format=".2f", default=1.0))
+  $(@bind a11 Scrubbable(δrange; format=".7f", default=δ))
+  $(@bind a12 Scrubbable(range_; format=".2f", default=1.0))
   ``)``
 
   ``(``
-  $(@bind c Scrubbable(range_; format=".2f", default=1.0))
-  $(@bind d Scrubbable(range_; format=".2f", default=1.0))
+  $(@bind a21 Scrubbable(range_; format=".2f", default=1.0))
+  $(@bind a22 Scrubbable(range_; format=".2f", default=1.0))
   ``)``
+
+  ``b =``
+  ``(``
+  $(@bind b1 Scrubbable(range_; format=".2f", default=1.0))
+  $(@bind b2 Scrubbable(range_; format=".2f", default=0.0))
+  ``)^T``
   """
 end
-
-# ╔═╡ 12c59d4c-0c5b-42b6-9192-54e0fa884647
-md"""
-``A_{0,0} =\,`` $a
-"""
 
 # ╔═╡ 87915587-8625-4081-8178-2a5f22689088
 md"""
 **Rmk.** 值得注意的是, 即便我們將 `a,b,c,d` 定義在以 `let` 起始的 cell 裏, 我們依然可以在其他 cells 裏使用同一個 `a,b,c,d`. 這是因爲 `@bind` 的緣故.
+"""
+
+# ╔═╡ c25b185b-76c7-4cd2-a62b-e326fd4bc2fd
+
+
+# ╔═╡ 32bb6296-2171-4680-9823-1a4d479d0328
+
+
+# ╔═╡ 0ecf5691-270e-407e-ad06-a01d6b68a52a
+
+
+# ╔═╡ 6a523141-94ae-461d-afbe-d6f951a15ad8
+let
+  range_ = -0:.001:1.5
+  md"""
+  ```math
+  \hat{A}x = \hat{b}
+  ```
+
+  ``\hat{A} =``
+
+  ``(``
+  $(@bind â11 Scrubbable(range_; format=".3f", default=0.661))
+  $(@bind â12 Scrubbable(range_; format=".3f", default=0.991))
+  ``)``
+
+  ``(``
+  $(@bind â21 Scrubbable(range_; format=".3f", default=0.500))
+  $(@bind â22 Scrubbable(range_; format=".3f", default=0.750))
+  ``)``
+
+  ``\hat{b} =``
+  ``(``
+  $(@bind b̂1 Scrubbable(range_; format=".3f", default=0.330))
+  $(@bind b̂2 Scrubbable(range_; format=".3f", default=0.250))
+  ``)^T``
+  """
+end
+
+
+# ╔═╡ 7b85f06c-6ef5-4341-9880-3e905379f3bb
+md"""
+試着調整 ``\hat{A}`` 或 ``\hat{b}`` 的值玩玩看. 有些組合會使得交點有大幅度的跳動, 
+這就是所謂的 $(HTML("<br>"))
+**ill-condtioned** problem/matrix.
+
 """
 
 # ╔═╡ 57843777-7dd2-4360-8acb-acfe4e83b870
@@ -88,47 +140,78 @@ end
 
 # ╔═╡ 700b7e6c-ba99-4d83-9d43-5e8f6c9d4593
 let
-  b1, b2 = 1, 0
   x = range(-2,0;step=0.2)
+  lw = 3
+  A = [a11 a12
+	   a21 a22]
+  b = [b1
+	   b2]
+  sol = A \ b
   plot(
     x,
-    y(a,b,b1).(x),
+    y(a11,a12,b1).(x),
     #linealpha=alpha,
-    #linewidth=lw,
+    linewidth=lw,
     xlim=(-2, 0),
     ylim=(0, 2),
     #xticks=range(0,1;length=M),
     #yticks=range(0,1;length=M),
     aspect_ratio=:equal,
-    #label="ϕ1",
+    label="$(a11)x + $(a12)y = $(b1)",
     #legend=:topleft,
     background_color=:black,
-    #title="M = $M",
+    title="well-conditioned",
   )
   plot!(
     x,
-    y(c,d,b2).(x),
+    y(a21,a22,b2).(x),
+    linewidth=lw,
+    label="$(a21)x + $(a22)y = $(b2)",
+  )
+  plot!(
+    sol[1:1],
+    sol[2:2],
+    marker=:o,
+    label="intersection",
   )
 end
 
-# ╔═╡ c25b185b-76c7-4cd2-a62b-e326fd4bc2fd
+# ╔═╡ c8840cd1-0ef6-40de-887e-49b2d1d5fd3c
 let
-  b1, b2 = 1, 0
-  x = range(-2,0;step=0.2)
+  l = 10
+  x = range(-l,l;step=0.2)
+  lw = 3
+  Â = [â11 â12
+	   â21 â22]
+  b̂ = [b̂1
+	   b̂2]
+  sol = Â \ b̂
   plot(
     x,
-    y(a,b,c).(x),
+    y(â11,â12,b̂1).(x),
     #linealpha=alpha,
-    #linewidth=lw,
-    #xlim=(0,1.1),
+    linewidth=lw,
+    xlim=(-l, l),
+    ylim=(-l, l),
     #xticks=range(0,1;length=M),
     #yticks=range(0,1;length=M),
     aspect_ratio=:equal,
-    #label="ϕ1",
-    label=false,
+    label="$(â11)x + $(â12)y = $(b̂1)",
     #legend=:topleft,
     background_color=:black,
-    #title="M = $M",
+    title="ill-conditioned",
+  )
+  plot!(
+    x,
+    y(â21,â22,b̂2).(x),
+    linewidth=lw,
+    label="$(â21)x + $(â22)y = $(b̂2)",
+  )
+  plot!(
+    sol[1:1],
+    sol[2:2],
+    marker=:o,
+    label="intersection",
   )
 end
 
@@ -136,11 +219,15 @@ end
 # ╠═26bd4dbe-b692-427a-acba-8e89544eeea6
 # ╠═ce66f0b6-d4ca-11eb-3d3a-79e2988c7492
 # ╠═7896a3ef-ff1f-4840-86c4-8ec2938936ed
-# ╟─ec6cddd4-e060-49c1-9080-cd254e0f3582
-# ╠═89a32942-b10c-4b07-b87a-a2a3cc1c2cdc
-# ╠═d9ce9c5d-cd96-478f-8efd-b0cf4cc0aca0
-# ╟─12c59d4c-0c5b-42b6-9192-54e0fa884647
-# ╠═700b7e6c-ba99-4d83-9d43-5e8f6c9d4593
+# ╠═ec6cddd4-e060-49c1-9080-cd254e0f3582
+# ╠═724c0e7a-8ec2-4f69-9142-5ecb4cb045b9
+# ╟─d9ce9c5d-cd96-478f-8efd-b0cf4cc0aca0
+# ╟─700b7e6c-ba99-4d83-9d43-5e8f6c9d4593
 # ╟─87915587-8625-4081-8178-2a5f22689088
 # ╠═c25b185b-76c7-4cd2-a62b-e326fd4bc2fd
+# ╠═32bb6296-2171-4680-9823-1a4d479d0328
+# ╠═0ecf5691-270e-407e-ad06-a01d6b68a52a
+# ╟─6a523141-94ae-461d-afbe-d6f951a15ad8
+# ╟─c8840cd1-0ef6-40de-887e-49b2d1d5fd3c
+# ╟─7b85f06c-6ef5-4341-9880-3e905379f3bb
 # ╠═57843777-7dd2-4360-8acb-acfe4e83b870
